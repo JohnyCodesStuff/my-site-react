efaf4c3d0e373a3d076004e19280000cc27dc1ce
JohnyCodesStuff <janpelczar12@gmail.com>
2024-08-04 21:43:01 +0200

    next commit - new functionality regarding Article

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Article from './components/Article';
 import ArticleBox from './components/ArticleBox';
 
 function App() {
   return (
     <div>
+      <Article title="Tytuł komponentu funkcyjnego" description="Opis funkcyjny 1" />
       <ArticleBox title="Tytuł 1" description="Opis 1" />
       <ArticleBox title="Tytuł 2" description="Opis 2" />
       <ArticleBox title="Tytuł 3" description="Opis 3" />
--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -1,3 +1,44 @@
-function Article(props) {
-    return <div>Article 1</div>
+import { useState } from "react";
+
+const Article = (props) => {
+    const [counter, setCounter] = useState(0);
+    const [timer, setTimer] = useState(100);
+    const [user, setUser] = useState({
+        name: "Patryk",
+        email: "patrykjankowski1212@gmail.com"
+    }
+)
+
+    const addCounter = () => {
+        setCounter(prevValue => prevValue + 1);
+    }
+
+    const addTimer = () => {
+        setTimer(prevValue => prevValue + 100);
+    }
+
+    console.log(user)
+
+    const changeName = () => {
+        setUser(prevValue => ({
+            ...prevValue,
+            name: "Adam"
+
+        }))
+    }    
+
+    return <div>
+        <h1>{props.title}</h1>
+        <p>{props.description}</p>
+        <p>{counter}</p>
+        <p>{timer}</p>
+        <p>{user.name}</p>
+        <button onClick={addCounter}>Dodaj</button>
+        <button onClick={addTimer}>Dodaj timer</button>
+        <button onClick={changeName}>Zmien imie</button>
+        
+       
+    </div>
 }
+// dodajemy tak zwane hooki
+export default Article;
