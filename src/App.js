a12eba0579f07f24ef5a717cce81966c3c057c89
JohnyCodesStuff <janpelczar12@gmail.com>
2024-08-06 01:33:15 +0200

    adding clock with updating time and a button

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@
 import './App.css';
 import Article from './components/Article';
 import ArticleBox from './components/ArticleBox';
+import Clock from './components/Clock';
 
 function App() {
   return (
     <div>
+      <Clock />
       <Article title="Tytuł komponentu funkcyjnego" description="Opis funkcyjny 1" />
       <ArticleBox title="Tytuł 1" description="Opis 1" />
       <ArticleBox title="Tytuł 2" description="Opis 2" />
       <ArticleBox title="Tytuł 3" description="Opis 3" />
+      <ArticleBox />
+      <div>
+        <p></p>
+      </div>
     </div>
   )
 }
--- src/components/Clock/index.js
+++ src/components/Clock/index.js
@@ -1,10 +1,84 @@
-import React from "react";
+/*import React from "react";
+import './style.css';
 
 class Clock extends React.Component{
     constructor(props) {
         super(props);
+
+        this.state = {
+            date: new Date(),
+            clicked: false
+        }
     }
+
+    componentDidMount() {
+        this.timerID = setInterval(() => this.changeTime(), 1000);           
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.clicked !== this.state.clicked) {
+            console.log('Huraaa!')
+        }
+    }
+
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    changeTime = () => {
+        this.setState({
+            date: new Date()
+        })
+    }
+
+
     render() {
-        return <div></div>;
+        return <div className="clock">
+            {this.state.date.toLocaleTimeString()}
+            <button onClick={() => this.setState({ clicked: true})}> Zmień </button>
+        </div>;
     }
 }
+
+export default Clock;
+
+
+*/
+
+import React, { useEffect, useState } from "react";
+import './style.css'
+
+const Clock = () => {
+
+    const [date, setDate] = useState(new Date());
+    const [clicked, setClicked] = useState(false);
+
+    const changeTime = () => {
+        setDate(new Date());
+    }
+
+    useEffect(() => {
+        const timerID = setInterval(changeTime, 1000);
+
+        return () => {
+            clearInterval(timerID)
+        }
+    }, [])
+
+    useEffect(() => {
+        console.log('HURAAA!')   
+    },[clicked])
+
+    console.log(date);
+    console.log(clicked);
+
+    return (
+        <div className="clock">
+            {date.toLocaleTimeString()}
+            <button onClick={() => setClicked(true)}>Zmień</button>
+        </div>
+    )
+}
+
+export default Clock
