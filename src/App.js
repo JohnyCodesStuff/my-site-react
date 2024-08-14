4f82082aba23c69e4a2dd57dca0aa25a1226abfd
JohnyCodesStuff <janpelczar12@gmail.com>
2024-08-14 23:30:32 +0200

    changing border size

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
             height="240"  
             src="https://www.youtube.com/embed/Z5NINTQfdJU?si=tb8oDj3U9r9rrH3S" 
             title="YouTube video player" 
-            frameborder="0" 
+            frameborder="10" 
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
             referrerpolicy="strict-origin-when-cross-origin" 
             justify-content="center"
