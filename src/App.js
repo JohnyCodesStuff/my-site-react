5e7268cc94c32c9d1cd215aad0af423da7ac279e
JohnyCodesStuff <janpelczar12@gmail.com>
2024-08-27 23:11:38 +0200

    simplifying the app to a good things counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 
 import './App.css';
-import Article from './components/Article';
-import ArticleBox from './components/ArticleBox';
 import Clock from './components/Clock';
-import HandleEvent from './components/HandleEvent';
 import soundbass from "./assets/soundbass.wav"
+import PersistentCounter from './components/PersistentCounter';
 
 
 function App() {
@@ -14,37 +12,9 @@ function App() {
   }   
 
   return (
-    <div>
-      <Clock />
-      <Article title="Tytuł komponentu funkcyjnego" description="Opis funkcyjny 1" />
-      <ArticleBox title="Tytuł 1" description="Opis 1" />
-      <ArticleBox title="Pusty artykuł"/>
-      <HandleEvent />
-      <div>
-        <p></p>
-      </div>
-      <div>
-        <p>
-        <button onClick={play}>
-          Włącz BASS
-        </button>
-        </p>
-      </div>
-      <div>
-        <p>
-        <iframe 
-            width="360" 
-            height="240"  
-            src="https://www.youtube.com/embed/Z5NINTQfdJU?si=tb8oDj3U9r9rrH3S" 
-            title="YouTube video player" 
-            frameborder="10" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-            referrerpolicy="strict-origin-when-cross-origin" 
-            justify-content="center"
-            allowfullscreen>
-        </iframe>
-        </p>
-      </div>
+    <div> 
+      <Clock />  
+      <PersistentCounter />
     </div>
   )
 }
---
+++ src/components/PersistentCounter/index.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+
+const PersistentCounter = () => {
+  // Retrieve the initial value from localStorage, or set it to 0 if not present
+  const initialCount = parseInt(localStorage.getItem('counter')) || 0;
+
+  // Initialize the state with the value from localStorage
+  const [count, setCount] = useState(initialCount);
+
+  // Update localStorage whenever the count changes
+  useEffect(() => {
+    localStorage.setItem('counter', count);
+  }, [count]);
+
+  // Handlers for incrementing and decrementing the count
+  function increment() {
+        setCount(prevCount => prevCount + 1);
+    }
+
+  const decrement = () => {
+    setCount(prevCount => prevCount - 1);
+    };
+
+    const backToZero = () => {
+    setCount(prevCount => prevCount = 0)
+    }
+
+  return (
+    <div>
+      <h1>Good Things Counter: {count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={backToZero}>Reset</button>
+    </div>
+  );
+};
+
+export default PersistentCounter;
