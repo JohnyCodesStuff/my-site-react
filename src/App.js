d353cdd480e3c1490c1f9f676f1aa8dc0294db0d
JohnyCodesStuff <janpelczar12@gmail.com>
2024-11-10 20:55:24 +0100

    rearranging the layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 
 import './App.css';
-import Clock from './components/Clock';
+/*import Clock from './components/Clock';*/
 import soundbass from "./assets/soundbass.wav"
+import Header from './components/Header';
 import PersistentCounter from './components/PersistentCounter';
 
 
 function App() {
 
-  function play() {
+  /*function play() {
     new Audio(soundbass).play()
-  }   
+  }   */
 
   return (
     <div> 
-      <Clock />  
+      <Header/>
       <PersistentCounter />
+    <div></div>  
     </div>
   )
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './style.css' 
+const Header = () => (
+    <div id="header">
+        <h1>Header</h1>
+    </div>
+)
+
+export default Header; 
